ae57c38c1bf2b87415c39d80797a8890dc7ceb2e
HuHamster <timofijglazov@gmail.com>
2025-05-06 19:42:07 +0300

    Add files via upload

---
+++ MotionTrigger/MotionTrigger.ino
@@ -0,0 +1,22 @@
+// Pins
+const int pirPin = 2; // Pin to which the PIR sensor output is connected
+const int ledPin = 13; // Built-in LED on the board
+
+void setup() {
+  pinMode(pirPin, INPUT); // PIR as input
+  pinMode(ledPin, OUTPUT); // LED as output
+  Serial.begin(9600); // Starting the serial monitor
+}
+
+void loop() {
+  int motionDetected = digitalRead(pirPin); // Reading the PIR status
+
+  if (motionDetected == HIGH) {
+    Serial.println("Motion detected!");
+    digitalWrite(ledPin, HIGH); // Turn on the LED
+  } else {
+    digitalWrite(ledPin, LOW); // Turn off the LED
+  }
+
+  delay(100); // Small delay for stability
+}
